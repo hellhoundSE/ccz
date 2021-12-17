027915d8083bcb131fb8fd79628fe80ee3c552e3
valuievruslan <valuevruslan1999@gmail.com>
2021-12-17 22:23:43 +0100

    sorry, i hate python

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            var runPipeline = new Pipeline();
+            string resourceGroup = "RuslanResourceGroup";
+            string dataFactoryName = "ruslandf";
+            if (args == null) {
+                Console.WriteLine("args is null");
+            } else {
+                runPipeline.StartPipeline(resourceGroup, dataFactoryName, args[0]);
+            }
+
+
+
+
+        }
+
+    }
+}
